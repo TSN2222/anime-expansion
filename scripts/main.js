0f0f629383f376f1c54b89d1424674db029cde9a
Nick Daniel <98364618+TSN2222@users.noreply.github.com>
2025-02-24 23:33:13 -0500

    Implement logic to display AniList data
    Displays the 24 most popular anime on the home page with minimal styles.

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,5 @@
-const query = `
+function getPopularAnime() {
+  const query = `
   query ($page: Int, $perPage: Int) {
     Page(page: $page, perPage: $perPage) {
       media(sort: POPULARITY_DESC, type: ANIME) {
@@ -20,24 +21,66 @@ const query = `
   }
 `;
 
-const variables = {
-  page: 1,
-  perPage: 24,
-};
+  const variables = {
+    page: 1,
+    perPage: 24,
+  };
 
-const options = {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json',
-    Accept: 'application/json',
-  },
-  body: JSON.stringify({
-    query,
-    variables,
-  }),
-};
+  const options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+    },
+    body: JSON.stringify({
+      query,
+      variables,
+    }),
+  };
 
-fetch('https://graphql.anilist.co', options)
-  .then((response) => response.json())
-  .then((data) => console.log(data))
-  .catch((err) => console.error(err));
+  fetch('https://graphql.anilist.co', options)
+    .then((response) => response.json())
+    .then((data) => displayPopularAnime(data))
+    .catch((err) => console.error(err));
+}
+
+function displayPopularAnime(data) {
+  const popularAnime = document.getElementById('main-item-two');
+  const animeSlice = data.data.Page.media.slice(0, 24);
+
+  animeSlice.forEach((item) => {
+    const englishTitle = item.title.english;
+    const releaseDate = item.startDate.year;
+    const coverImage = item.coverImage.large;
+    const coverImageColor = item.coverImage.color;
+    const mediaFormat = item.format;
+    const rating = item.averageScore;
+    const episodeCount = item.episodes;
+
+    const popularAnimeHTML = `
+      <a href="">
+        <div>
+          <div>
+            <img
+              id="coverImage"
+              src="${coverImage}"
+              alt="Cover image of ${englishTitle}" />
+          </div>
+          <div>
+            <div>${englishTitle}</div>
+            <div id="info-container">
+              <div>${mediaFormat}</div>
+              <div>${releaseDate}</div>
+              <div>${episodeCount}</div>
+              <div>${rating}</div>
+            </div>
+          </div>
+        </div>
+      </a>
+    `;
+
+    popularAnime.innerHTML += popularAnimeHTML;
+  });
+}
+
+getPopularAnime();
