fc3ce5d33fc6a4debecca3264571be1d8b99dab7
Jet <jetk2311@users.noreply.github.com>
2025-03-16 15:27:27 +0000

    Select DOM elements for sidebar links

---
+++ scripts/sidebar.js
@@ -0,0 +1,4 @@
+// Sidebar menu item interaction
+const menuItems = document.querySelectorAll(".sidebar a");
+const homeContent = document.getElementById("home-content");
+const homeCarousel = document.getElementById("home-carousel");
