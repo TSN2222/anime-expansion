9868484b51b2b39b9d9f86ee356affb340d1a28e
Nick Daniel <98364618+TSN2222@users.noreply.github.com>
2025-03-25 16:44:38 -0400

    Add basic template for two settings

---
+++ scripts/settings.js
@@ -0,0 +1,19 @@
+const button = document.querySelector('.dropbtn');
+
+button.addEventListener('click', () => {
+  document.getElementById('myDropdown').classList.toggle('show');
+});
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function (event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName('dropdown-content');
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+};
